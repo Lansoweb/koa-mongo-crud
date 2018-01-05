e1e75ad1c7a2606c620882bde5d0401aac3754db
Leandro Silva <leandro.silva@zoox.com.br>
2018-01-05 10:45:02 -0200

    First release

---
+++ index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  CrudController: require('./crud/controller'),
+  CrudMapper: require('./crud/mapper'),
+  ValidationException: require('./crud/validation-exception'),
+  ResponseTimeMiddleware: require('./middleware/response-time'),
+  ErrorMiddleware:  require('./middleware/error'),
+  AuthMiddleware:  require('./middleware/auth')
+};
