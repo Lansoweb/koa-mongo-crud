7841b5b46e33d40e978e06a88cb4378e4eb2b854
Leandro Silva <leandro.silva@zoox.com.br>
2018-01-08 16:38:06 -0200

    Adding newrelic error middleware

---
+++ middleware/newrelic-error.js
@@ -0,0 +1,17 @@
+const newrelic = require('newrelic');
+
+const DEFAULT_TRANSACTION_NAME = (method, path) => 'Koajs/' + (path[0] === '/' ? path.slice(1) : path) + '#' + method;
+
+module.exports = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (typeof err.errors === 'object') {
+      err.status = 422;
+    }
+    newrelic.noticeError(err);
+    newrelic.setTransactionName(DEFAULT_TRANSACTION_NAME(ctx.method, ctx._matchedRoute));
+    throw err;
+  }
+  newrelic.setTransactionName(DEFAULT_TRANSACTION_NAME(ctx.method, ctx._matchedRoute));
+};
