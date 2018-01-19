27195d2d4ca3e4299c25fc34e9795e84717043d4
Leandro Silva <leandro.silva@zoox.com.br>
2018-01-19 17:14:34 -0200

    Exporting mongodb-query-filter

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ module.exports = {
   NewRelicMiddleware:  require('./middleware/newrelic'),
   AuthMiddleware:  require('./middleware/auth'),
   ApiServer:  require('./server'),
-  Uuid: require('./infra/uuid')
+  Uuid: require('./infra/uuid'),
+  MongoQF: require('./crud/mongodb-query-filter')
 };
